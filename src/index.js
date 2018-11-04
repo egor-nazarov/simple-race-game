e16c472dcd7b8da7731d2508b049998463f68948
Egor Elkin <elkinegora@gmail.com>
2018-11-04 13:59:31 +0500

    добавил в собранный проект элементы из рекомендованного (Racing Pack), добавил объект landscape, который отображает траву на поле

--- src/index.js
+++ src/index.js
@@ -16,10 +16,50 @@ var game = new Phaser.Game(config);
 function preload ()
 {
     this.load.image('logo', 'assets/logo.png');
+    this.load.image('grass', 'assets/Tiles/Grass/land_grass04.png');
 }
 
 function create ()
 {
+    /* создаю травянной фон на поле */
+    var landscape = [
+        this.add.image(0, 0, 'grass').setOrigin(0, 0),
+        this.add.image(128, 0, 'grass').setOrigin(0, 0),
+        this.add.image(256, 0, 'grass').setOrigin(0, 0),
+        this.add.image(384, 0, 'grass').setOrigin(0, 0),
+        this.add.image(512, 0, 'grass').setOrigin(0, 0),
+        this.add.image(640, 0, 'grass').setOrigin(0, 0),
+        this.add.image(768, 0, 'grass').setOrigin(0, 0),
+        this.add.image(0, 128, 'grass').setOrigin(0, 0),
+        this.add.image(128, 128, 'grass').setOrigin(0, 0),
+        this.add.image(256, 128, 'grass').setOrigin(0, 0),
+        this.add.image(384, 128, 'grass').setOrigin(0, 0),
+        this.add.image(512, 128, 'grass').setOrigin(0, 0),
+        this.add.image(640, 128, 'grass').setOrigin(0, 0),
+        this.add.image(768, 128, 'grass').setOrigin(0, 0),
+        this.add.image(0, 256, 'grass').setOrigin(0, 0),
+        this.add.image(128, 256, 'grass').setOrigin(0, 0),
+        this.add.image(256, 256, 'grass').setOrigin(0, 0),
+        this.add.image(384, 256, 'grass').setOrigin(0, 0),
+        this.add.image(512, 256, 'grass').setOrigin(0, 0),
+        this.add.image(640, 256, 'grass').setOrigin(0, 0),
+        this.add.image(768, 256, 'grass').setOrigin(0, 0),
+        this.add.image(0, 384, 'grass').setOrigin(0, 0),
+        this.add.image(128, 384, 'grass').setOrigin(0, 0),
+        this.add.image(256, 384, 'grass').setOrigin(0, 0),
+        this.add.image(384, 384, 'grass').setOrigin(0, 0),
+        this.add.image(512, 384, 'grass').setOrigin(0, 0),
+        this.add.image(640, 384, 'grass').setOrigin(0, 0),
+        this.add.image(768, 384, 'grass').setOrigin(0, 0),
+        this.add.image(0, 512, 'grass').setOrigin(0, 0),
+        this.add.image(128, 512, 'grass').setOrigin(0, 0),
+        this.add.image(256, 512, 'grass').setOrigin(0, 0),
+        this.add.image(384, 512, 'grass').setOrigin(0, 0),
+        this.add.image(512, 512, 'grass').setOrigin(0, 0),
+        this.add.image(640, 512, 'grass').setOrigin(0, 0),
+        this.add.image(768, 512, 'grass').setOrigin(0, 0),
+    ];
+
     var logo = this.add.image(400, 150, 'logo');
 
     this.tweens.add({
